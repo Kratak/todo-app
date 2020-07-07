0e66182fabc4ca8b0619a74f956b988f6e60425f
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2020-07-08 01:20:25 +0200

    + Add: Basic Login screen styles

--- src/components/pages/login/login.tsx
+++ src/components/pages/login/login.tsx
@@ -1,3 +1,31 @@
 import React from "react";
+import PaperBase from "@material-ui/core/Paper";
+import styled from "styled-components";
 
-export const Login = () => <div>Login</div>;
+import { stylesConstans } from "components/shared/utils";
+
+const { media, colors, compoundStyles } = stylesConstans;
+
+const Paper = styled(PaperBase)`
+  && {
+    align-self: center;
+    width: 90vw;
+    min-height: 200px;
+    transform: translateX(-50%);
+    position: absolute;
+    top: 80px;
+    left: 50%;
+    background-color: ${colors.modalBackground};
+    border: ${compoundStyles.border};
+
+    ${media.tabletOnly} {
+      width: 400px;
+    }
+
+    ${media.desktopOnly} {
+      width: 300px;
+    }
+  }
+`;
+
+export const Login = () => <Paper>Login</Paper>;
