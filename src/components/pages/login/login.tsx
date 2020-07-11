eeda86e6787649ec44a1c12f7712398956c5215a
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2020-07-12 00:30:46 +0200

    + Add: onLogin actions, re-route

--- src/components/pages/login/login.tsx
+++ src/components/pages/login/login.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 
+import { staticRoutes } from "logics/router";
+
 import { Paper, TextField, GridItem } from "./styled";
 
-export const Login = () => {
+interface ILoginProps extends RouteComponentProps {}
+
+export const Login = (props: ILoginProps) => {
   const isValid = true;
   const errorMessage = "Incorrect entry.";
   const helperText = "*Field Required";
@@ -13,6 +18,10 @@ export const Login = () => {
   const loginText = "Login";
 
   const helperTextMessage = isValid ? helperText : errorMessage;
+
+  const onLogin = () => {
+    props.history.push(staticRoutes.list);
+  };
 
   return (
     <Paper>
@@ -33,7 +42,9 @@ export const Login = () => {
           />
         </GridItem>
         <GridItem>
-          <Button variant={"contained"}>{loginText}</Button>
+          <Button variant={"contained"} onClick={onLogin}>
+            {loginText}
+          </Button>
         </GridItem>
       </Grid>
     </Paper>
