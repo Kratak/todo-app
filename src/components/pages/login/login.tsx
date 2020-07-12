026eb5300898e5a098204e9ef3f6cc4248d4e8ec
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2020-07-13 00:09:40 +0200

    + Add: [ISSUE-5] i18n implementation

--- src/components/pages/login/login.tsx
+++ src/components/pages/login/login.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { RouteComponentProps } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import Grid from "@material-ui/core/Grid";
 
 import { staticRoutes } from "logics/router";
+import { ELoginMessagesKeys, ENamesSpaces } from "resources/translation/keys";
 
 import { Paper, TextField, GridItem, LoginButton } from "./styled";
 
 interface ILoginProps extends RouteComponentProps {}
 
 export const Login = (props: ILoginProps) => {
+  const { t } = useTranslation(ENamesSpaces.LoginMessage);
+
   const isValid = true;
   const errorMessage = "Incorrect entry.";
   const helperText = "*Field Required";
   const loginLabel = "Login";
   const passwordLabel = "Password";
-  const loginText = "Sign in";
 
   const helperTextMessage = isValid ? helperText : errorMessage;
 
@@ -41,7 +44,9 @@ export const Login = (props: ILoginProps) => {
           />
         </GridItem>
         <GridItem>
-          <LoginButton onClick={onLoginSubmit}>{loginText}</LoginButton>
+          <LoginButton onClick={onLoginSubmit}>
+            {t(ELoginMessagesKeys.LoginButton)}
+          </LoginButton>
         </GridItem>
       </Grid>
     </Paper>
